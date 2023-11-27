3d2fb52168b4d5b510794769c43801ba4aaa6617
NFHr <expfoahh@outlook.com>
2023-11-27 23:51:35 +0800

    mcdecoder

--- src_files/mcdecoder.vhd
+++ src_files/mcdecoder.vhd
@@ -14,123 +14,120 @@ ENTITY mcdecoder IS
 END mcdecoder;
 
 ARCHITECTURE Behavioral OF mcdecoder IS
-  TYPE state_type IS (St_ERROR, St_WAIT_BOS, St_DECODE, St_DECODE_FIN, St_VALID);
-  SIGNAL next_state, state : state_type;
-  SIGNAL din_reg : STD_LOGIC_VECTOR(11 DOWNTO 0) := (OTHERS => '0');
-  SIGNAL dout_cnt : STD_LOGIC_VECTOR(1 DOWNTO 0);
-  SIGNAL dout_reg : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+  TYPE state_type IS (St_RESET, St_ERROR, St_WAIT_BOS, St_DECODE, St_DECODE_FIN, St_DECODE_OK);
+  SIGNAL next_state, state : state_type := St_RESET;
+
+  SIGNAL prev_valid : STD_LOGIC;
+  SIGNAL digits : STD_LOGIC_VECTOR(5 DOWNTO 0);
+  SIGNAL index, prev_index : STD_LOGIC := '1';
+
 BEGIN
 
   SYNC_PROC : PROCESS (clk, clr)
   BEGIN
-    IF (clr = '1') THEN
-      state <= St_WAIT_BOS;
+    IF clr = '1' THEN
+      state <= St_RESET;
     ELSIF rising_edge(clk) THEN
       state <= next_state;
+      prev_index <= index;
+      IF (valid = '1' AND prev_valid = '0') THEN
+        digits <= digits(2 DOWNTO 0) & din;
+        index <= (state = St_RESET) ? '1' : NOT index;
+      END IF;
+      prev_valid <= valid;
     END IF;
   END PROCESS;
 
-  NEXT_STATE_PROC : PROCESS (state, din_reg)
-    VARIABLE eos_flag : STD_LOGIC := '0';
+  NEXT_STATE_PROC : PROCESS (state, digits)
   BEGIN
-    CASE state IS
-      WHEN St_VALID =>
-        next_state <= St_DECODE;
-      WHEN St_ERROR =>
-        next_state <= St_WAIT_BOS;
-      WHEN St_WAIT_BOS =>
-        IF din_reg = "000111000111" THEN
-          next_state <= St_DECODE;
+    CASE(state) IS
+      WHEN St_RESET =>
+      IF digits = "000111" THEN
+        nest_state <= St_STARTING;
+      END IF;
+      WHEN St_STARTING =>
+      IF (index = '1' AND prev_index = '0') THEN
+        IF digits = "000111" THEN
+          next_state <= St_RESET;
+        ELSE
+          next_state <= St_ERROR;
         END IF;
-      WHEN St_DECODE =>
-        IF dout_cnt = "01" THEN
-          next_state <= St_DECODE_FIN;
+      END IF;
+      WHEN St_LISTENING =>
+      IF (index = '1' AND prev_index = '0') THEN
+        IF note_byte = "111000" THEN
+          next_state <= St_ENDDING;
+        ELSE
+          next_state <= St_WRITING;
         END IF;
-      WHEN St_DECODE_FIN =>
-        IF dout_cnt = "10" THEN
-          next_state <= St_VALID;
-          CASE din_reg(5 DOWNTO 0) IS
-              --1x
-            WHEN "001010" => dout_reg <= "01000010";--B
-            WHEN "001011" => dout_reg <= "01000100";--D
-            WHEN "001100" => dout_reg <= "01001000";--H
-            WHEN "001101" => dout_reg <= "01001100";--L
-            WHEN "001110" => dout_reg <= "01010010";--R
-              --2x
-            WHEN "010001" => dout_reg <= "01000001";--A
-            WHEN "010011" => dout_reg <= "01000111";--G
-            WHEN "010100" => dout_reg <= "01001011";--K
-            WHEN "010101" => dout_reg <= "01010001";--Q
-            WHEN "010110" => dout_reg <= "01010110";--V
-              --3x
-            WHEN "011001" => dout_reg <= "01000011";--C
-            WHEN "011010" => dout_reg <= "01000110";--F
-            WHEN "011100" => dout_reg <= "01010000";--P
-            WHEN "011101" => dout_reg <= "01010101";--U
-            WHEN "011110" => dout_reg <= "01011010";--Z
-              --4x
-            WHEN "100001" => dout_reg <= "01000101";--E
-            WHEN "100010" => dout_reg <= "01001010";--J
-            WHEN "100011" => dout_reg <= "01001111";--O
-            WHEN "100101" => dout_reg <= "01011001";--Y
-            WHEN "100110" => dout_reg <= "00101110";--. 
-              --5x
-            WHEN "101001" => dout_reg <= "01001001";--I
-            WHEN "101010" => dout_reg <= "01001110";--N
-            WHEN "101011" => dout_reg <= "01010100";--T
-            WHEN "101100" => dout_reg <= "01011000";--X
-            WHEN "101110" => dout_reg <= "00111111";--?
-              --6x
-            WHEN "110001" => dout_reg <= "01001101";--M
-            WHEN "110010" => dout_reg <= "01010011";--S
-            WHEN "110011" => dout_reg <= "01010111";--W
-            WHEN "110100" => dout_reg <= "00100001";--!
-            WHEN "110101" => dout_reg <= "00100000";--SPACE
-            WHEN OTHERS =>
-              IF din_reg(5 DOWNTO 0) = "111000" THEN
-                IF eos_flag = '1' THEN
-                  next_state <= St_WAIT_BOS;
-                  eos_flag := '0';
-                ELSE
-                  eos_flag := '1';
-                  next_state <= St_DECODE;
-                END IF;
-              ELSE
-                next_state <= St_ERROR;
-              END IF;
-          END CASE;
+      END IF;
+      WHEN St_WRITING => next_state <= St_LISTENING;
+      WHEN St_ENDDING =>
+      IF (index = '1' AND prev_index = '0') THEN
+        IF note_byte = "111000" THEN
+          next_state <= St_RESET;
+        ELSE
+          next_state <= St_ERROR;
         END IF;
-      WHEN OTHERS => next_state <= state;
+      END IF;
+      WHEN St_ERROR => next_state <= St_RESET;
+      WHEN OTHERS => next_state <= St_RESET;
     END CASE;
   END PROCESS;
 
-  DIN_PROC : PROCESS (valid)
-  BEGIN
-    IF valid = '1' THEN
-      din_reg <= din_reg(8 DOWNTO 0) & din;
-      IF state = St_DECODE THEN
-        dout_cnt <= "01";
-      ELSIF state = St_DECODE_FIN THEN
-        dout_cnt <= "10";
-      END IF;
-    ELSE
-      dout_cnt <= "00";
-    END IF;
-  END PROCESS;
-
-  OUTPUT_PROC : PROCESS (state)
+  OUTPUT_LOGIC : PROCESS (state)
   BEGIN
     CASE state IS
       WHEN St_ERROR =>
         error <= '1';
         dvalid <= '0';
-      WHEN St_VALID =>
-        dout <= dout_reg;
-        dvalid <= '1';
+      WHEN St_WRITING =>
         error <= '0';
+        dvalid <= '1';
+        CASE digits IS
+          WHEN "001010" => dout <= "01000010";--B
+          WHEN "001011" => dout <= "01000100";--D
+          WHEN "001100" => dout <= "01001000";--H
+          WHEN "001101" => dout <= "01001100";--L
+          WHEN "001110" => dout <= "01010010";--R
+
+          WHEN "010001" => dout <= "01000001";--A
+          WHEN "010011" => dout <= "01000111";--G
+          WHEN "010100" => dout <= "01001011";--K
+          WHEN "010101" => dout <= "01010001";--Q
+          WHEN "010110" => dout <= "01010110";--V
+
+          WHEN "011001" => dout <= "01000011";--C
+          WHEN "011010" => dout <= "01000110";--F
+          WHEN "011100" => dout <= "01010000";--P
+          WHEN "011101" => dout <= "01010101";--U
+          WHEN "011110" => dout <= "01011010";--Z
+
+          WHEN "100001" => dout <= "01000101";--E
+          WHEN "100010" => dout <= "01001010";--J
+          WHEN "100011" => dout <= "01001111";--O
+          WHEN "100101" => dout <= "01011001";--Y
+          WHEN "100110" => dout <= "00101110";--. 
+
+          WHEN "101001" => dout <= "01001001";--I
+          WHEN "101010" => dout <= "01001110";--N
+          WHEN "101011" => dout <= "01010100";--T
+          WHEN "101100" => dout <= "01011000";--X
+          WHEN "101110" => dout <= "00111111";--?
+
+          WHEN "110001" => dout <= "01001101";--M
+          WHEN "110010" => dout <= "01010011";--S
+          WHEN "110011" => dout <= "01010111";--W
+          WHEN "110100" => dout <= "00100001";--!
+          WHEN "110101" => dout <= "00100000";--SPACE
+
+          WHEN OTHERS => dout <= "00000000";
+        END CASE;
       WHEN OTHERS =>
         error <= '0';
         dvalid <= '0';
     END CASE;
   END PROCESS;
+
 END Behavioral;
